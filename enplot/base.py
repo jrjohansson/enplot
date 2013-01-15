f323f3d153cb9ab848d41a02ce71b094e94a5e3c
Robert Johansson <jrjohansson@gmail.com>
2013-01-15 20:07:30 +0900

    fixed pep8 incompatibilities

--- enplot/base.py
+++ enplot/base.py
@@ -21,9 +21,7 @@ import sys
 
 debug = 0
 
-# ------------------------------------------------------------------------------
-# 
-#
+
 def data_matrix_sort(M, col):
     """
     Sort the columns in a given matrix according to give column.
@@ -31,32 +29,31 @@ def data_matrix_sort(M, col):
 
     MM = copy.copy(M)
 
-    v = M[:,col]
+    v = M[:, col]
     vv = copy.copy(sort(v))
 
     for i in range(0, len(vv)):
         for j in range(0, len(vv)):
-            if M[j,col] != vv[i]:
+            if M[j, col] != vv[i]:
                 j += 1
             else:
                 break
 
-            MM[i,:] = M[j,:]
+            MM[i, :] = M[j, :]
 
     return MM
 
-# ------------------------------------------------------------------------------
-# 
-#
-def data_matrix_read_file(filename, sep = "\t"):
+
+def data_matrix_read_file(filename, sep="\t"):
     """
     Read a matrix from a field-separated values text file (by default tab
-    separated, but the separator can be changed by the optional second argument)
+    separated, but the separator can be changed by the optional second
+    argument)
     """
 
     N = 0
     M = 0
-    
+
     file = open(filename, "r")
     for line in file.readlines():
         M += 1
@@ -66,8 +63,8 @@ def data_matrix_read_file(filename, sep = "\t"):
 
     if debug != 0:
         print "Found matrix of size MxN =", M, "x", N
-    mat = zeros((M,N), float)
-    
+    mat = zeros((M, N), float)
+
     file = open(filename, "r")
     m = 0
     for line in file.readlines():
@@ -77,32 +74,30 @@ def data_matrix_read_file(filename, sep = "\t"):
         elemlist = line.strip().split(sep)
         for e in elemlist:
             if e:
-                mat[m,n] = float(e)
+                mat[m, n] = float(e)
             else:
-                mat[m,n] = 0
+                mat[m, n] = 0
             n += 1
         m += 1
     file.close
 
-    return mat, N, M 
+    return mat, N, M
 
-# ------------------------------------------------------------------------------
-# Read matrix data from a file
-#
+
 def file_data_read(datafile, sep=None):
     """
-    Read data from a field-separated values text file (by default tab separated,
-    but the separator can be changed by the optional second argument). Return
-    the data in matrix.
+    Read data from a field-separated values text file (by default tab
+    separated, but the separator can be changed by the optional second
+    argument). Return the data in matrix.
     """
 
-    if datafile == None:
+    if datafile is None:
         raise ValueError("datafile is unspecified")
 
     f = open(datafile, "r")
 
     #
-    # first count lines and numbers of 
+    # first count lines and numbers of
     #
     M = N = 0
     for line in f:
@@ -110,7 +105,7 @@ def file_data_read(datafile, sep=None):
         if line[0] == '#' or line[0] == '%':
             continue
         # find delim
-        if N == 0 and sep == None:
+        if N == 0 and sep is None:
             if len(line.rstrip().split(",")) > 1:
                 sep = ","
             elif len(line.rstrip().split(";")) > 1:
@@ -120,7 +115,7 @@ def file_data_read(datafile, sep=None):
             elif len(line.rstrip().split("|")) > 1:
                 sep = "|"
             elif len(line.rstrip().split()) > 1:
-                sep = None # sepical case for a mix of white space deliminators
+                sep = None  # for a mix of white space deliminators
             else:
                 raise ValueError("Unrecognized column deliminator")
         # split the line
@@ -141,7 +136,8 @@ def file_data_read(datafile, sep=None):
                 numformat = "decimal"
 
         elif N != n:
-            raise ValueError("Badly formatted data file: unequal number of columns")
+            raise ValueError(
+                "Badly formatted data file: unequal number of columns")
         M += 1
 
     #
@@ -150,7 +146,7 @@ def file_data_read(datafile, sep=None):
     f.seek(0)
 
     if numtype == "complex":
-        data = zeros((M,N), dtype="complex")
+        data = zeros((M, N), dtype="complex")
         m = n = 0
         for line in f:
             # skip comment lines
@@ -158,12 +154,12 @@ def file_data_read(datafile, sep=None):
                 continue
             n = 0
             for item in line.rstrip().split(sep):
-                data[m,n] = complex(item)
+                data[m, n] = complex(item)
                 n += 1
             m += 1
-    
+
     else:
-        data = zeros((M,N), dtype="float")        
+        data = zeros((M, N), dtype="float")
         m = n = 0
         for line in f:
             # skip comment lines
@@ -171,7 +167,7 @@ def file_data_read(datafile, sep=None):
                 continue
             n = 0
             for item in line.rstrip().split(sep):
-                data[m,n] = float(item)
+                data[m, n] = float(item)
                 n += 1
             m += 1
 
@@ -179,13 +175,11 @@ def file_data_read(datafile, sep=None):
 
     return data, M, N
 
-# ------------------------------------------------------------------------------
-# 
-#
+
 def build_Z(M, x_range, y_range, xcol, ycol, zcol):
     """
     Constuct a matrix representation of data in list format.
-    """    
+    """
 
     n = x_range.size
     m = y_range.size
@@ -193,13 +187,11 @@ def build_Z(M, x_range, y_range, xcol, ycol, zcol):
     if debug != 0:
         print "Building Z [" + str(n) + "x" + str(m) + "]"
 
-    Z = np.zeros((m,n), float)
+    Z = np.zeros((m, n), float)
 
-    for i in range(len(M[:,zcol])):
-        a = np.nonzero(x_range == M[i,xcol])[0][0]
-        b = np.nonzero(y_range == M[i,ycol])[0][0]
-        Z[b, a] = M[i,zcol]
+    for i in range(len(M[:, zcol])):
+        a = np.nonzero(x_range == M[i, xcol])[0][0]
+        b = np.nonzero(y_range == M[i, ycol])[0][0]
+        Z[b, a] = M[i, zcol]
 
     return Z
-
-
